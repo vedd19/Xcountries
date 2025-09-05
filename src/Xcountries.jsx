2e2458cb6f012425e8b8e179e75f10f4ee3d47c8
vedd19 <ved123official@gmail.com>
2025-09-05 22:54:08 +0530

    ...

--- src/Xcountries.jsx
+++ src/Xcountries.jsx
@@ -4,9 +4,9 @@ export default function Xcountries() {
 
     const Card = ({ flag, name }) => {
         return (
-            <div className="card" style={{ height: 200, width: 200, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: 4 }}>
+            <div className="card" style={{ height: 200, width: 200, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", border: "1px solid gray", borderRadius: "8px", textAlign: "center" }}>
                 <img src={flag} alt="Country Flag" style={{ height: 100, width: 100 }} />
-                <h2>{name}</h2>
+                <h2 style={{ margin: "0", padding: "4px 0" }}>{name}</h2>
 
             </div>
         );
@@ -16,31 +16,31 @@ export default function Xcountries() {
 
     useEffect(() => {
 
-        async function fetchData() {
-            try {
-                const res = await fetch('https://xcountries-backend.azurewebsites.net/all');
-                const data = await res.json();
-                setCountries(data);
-            }
-            catch (err) {
-                console.error("Error fetching data: ", err)
-            }
+        // async function fetchData() {
+        //     try {
+        //         const res = await fetch('https://xcountries-backend.azurewebsites.net/all');
+        //         const data = await res.json();
+        //         setCountries(data);
+        //     }
+        //     catch (err) {
+        //         console.error("Error fetching data: ", err)
+        //     }
 
-        }
+        // }
 
-        fetchData()
+        // fetchData()
         // }
         // try {
 
-        //     fetch('https://xcountries-backend.azurewebsites.net/al').then(res => res.json()).then(data => setCountries(data))
+        fetch('https://xcountries-backend.azurewebsites.net/all').then(res => res.json()).then(data => setCountries(data)).catch(err => console.error("Error fetching data : ", err))
         // } catch (err) {
         //     console.error("Error in fetching data : ", err)
         // }
     }, [])
 
     return (
-        <div style={{ display: 'flex', flexWrap: 'wrap', gap: 10, justifyContent: 'center' }}>
-            {countries.map(country => <Card key={country.name + country.abbr} flag={country.flag} name={country.name} />)}
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: 18, justifyContent: 'center', alignItems: "center" }}>
+            {countries.map(country => <Card key={country.name + country.abbr + Math.random()} flag={country.flag} name={country.name} />)}
         </div>
     );
 }
